8c351febd173320db91778cc4d2a665465fa05a8
JackJosephWright <jwrightzz1234@gmail.com>
2022-01-05 08:39:26 -0500

    make it so i dont have to measure length each time to gen random list items

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 def gen_random_pics(db):
-    n_pics = len(db.trees)
+    n_pics = db.n_pics
     r_list = sample(range(1,n_pics+1),2)
                 
                 
